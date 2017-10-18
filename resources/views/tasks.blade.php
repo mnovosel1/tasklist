be254837c5af182ce526d9a76204a17e30efa6d8
marijo <marijo.novosel@predavaci.algebra.hr>
2017-10-18 21:10:28 +0200

    napravljen view tasks

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -8,6 +8,25 @@
 		<!-- WebObrazac za novi Task -->
 		<form action="/task" method="POST" class="form-horizontal">
 			{{ csrf_field() }}
+			
+			<!-- Task naziv -->
+			<div class="form-group">
+				<label for="task" class="col-sm-3 control-label">Task</label>
+				
+				<div class="col-sm-6">
+					<input type="text" name="name" id="task-name" class="form-control">
+				</div>
+			</div>
+			
+			<!-- Gumb za dodavanje taska -->
+			<div class="form-group">
+				<div class="col-sm-offset-3 col-sm-6">
+					<button type="submit" class="btn btn-default">
+						<i class="fa fa-plus"></i> Dodaj task
+					</button>
+				</div>
+			</div>
+			
 		</form>
 	</div>
 @endsection
